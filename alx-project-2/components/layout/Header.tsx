617bc48cf91e962a19db06ba1e1dcacced681246
Matthsworld <Matthsworld@gmail.com>
2025-01-07 20:32:09 +0100

    add: Initial Next.js project setup

---
+++ alx-project-2/components/layout/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+      <header>
+        <nav>
+          <h1>ALX Project 2</h1>
+        </nav>
+      </header>
+    );
+  };
+  
+  export default Header;
+  
---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/layout/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Welcome to the ALX Project Setup</h1>
+        <p>Get started by editing the files in the alx-project-2 directory.</p>
+      </main>
+    </div>
+  );
+}
